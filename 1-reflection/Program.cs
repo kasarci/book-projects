a6db8127f581925ad6837059014b5a9bb726dc8e
kasarci <muhammedkasarci@hotmail.com>
2022-09-05 21:27:16 +0200

    Created reflection project to understand how to get types and methods in the assemblies.

---
+++ 1-reflection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+Assembly? assembly = Assembly.GetEntryAssembly();
+
+foreach (AssemblyName name in assembly.GetReferencedAssemblies())
+{
+  Assembly a = Assembly.Load(name);
+  int methodCount = 0;
+
+  foreach (TypeInfo t in a.DefinedTypes)
+  {
+    methodCount += t.GetMethods().Length;
+  }
+
+  Console.WriteLine(
+    "{0:N0} types with {1:N0}, methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount,
+    arg2: name.Name
+  );
+}
