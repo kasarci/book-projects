745e2e8bb6fe37edc7f9b5b2250ab3e3bda10ef2
kasarci <muhammedkasarci@hotmail.com>
2022-09-06 21:07:14 +0200

    Created a project to see how string base 64 encodings work.

---
+++ 3-binary-to-string/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+byte[] byteArray = new byte[128];
+
+new Random().NextBytes(byteArray); // NextBytes method fills the elements of a specified array of bytes with random numbers.
+
+Console.WriteLine("Binary object as bytes: ");
+
+for(int i  = 0; i < byteArray.Length; i++) {
+  Console.Write($"{byteArray[i]:X} ");
+}
+Console.WriteLine();
+
+string encoded = Convert.ToBase64String(byteArray);
+
+Console.WriteLine(encoded);
+
+var decodedCharArray = Convert.FromBase64String(encoded);
+
+for (int i = 0; i < decodedCharArray.Length; i++)
+{
+  Console.Write($"{decodedCharArray[i]:X} ");
+}
