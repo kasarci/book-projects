e5a3a750acf59ddb24eac9f48ed4f412c7fadfab
kasarci <muhammedkasarci@hotmail.com>
2022-09-09 22:14:22 +0200

    Created a project to understand tuples.

---
+++ 7-tuples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Contracts;
+using static System.Console;
+
+// Fields of tuples has got their names automatically as Item1, Item2... when they are leaved empty.
+WriteLine($"I have {GetFruits().Item2} {GetFruits().Item1}.");
+//Output: I have 3 apples.
+
+WriteLine($"I also have {GetFruitsWithFieldNames().Count} {GetFruitsWithFieldNames().Name}.");
+//Output: I also have 5 oranges.
+
+//simple method returning (string, int) tuple
+(string, int) GetFruits() {
+  return ("apples", 3);
+}
+
+// Naming tuples.
+(string Name, int Count) GetFruitsWithFieldNames () {
+  return ("oranges", 5);
+}
