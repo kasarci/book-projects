29275b9898ce0351ae36f353a0e0fdd85dbf2d30
kasarci <muhammedkasarci@hotmail.com>
2022-09-16 21:33:47 +0200

    Added a method to see how to query database with EFCore.

--- 14-efcore-northwind/Program.cs
+++ 14-efcore-northwind/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello From EFCore!");
+﻿using _14_efcore_northwind;
+using _14_efcore_northwind.Entities;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello From EFCore!");
+QueryingCategories();
+
+void QueryingCategories() {
+  using(Northwind db = new Northwind()) {
+    Console.WriteLine("Categorories and how many products they have");
+    IQueryable<Category>? categories = db.Categories.Include(c => c.Products);
+
+    if(categories is null) {
+      Console.WriteLine("No categories found.");
+      return;
+    }
+
+    foreach (Category category in categories)
+    {
+      Console.WriteLine($"{category.CategoryName} has {category.Products.Count} products.");
+    }
+  }
+}
