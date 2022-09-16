d38644738d13841069253e776b84115e693a2377
kasarci <muhammedkasarci@hotmail.com>
2022-09-16 21:53:55 +0200

    Created a method for adding filters to database queries.

--- 14-efcore-northwind/Program.cs
+++ 14-efcore-northwind/Program.cs
@@ -1,9 +1,11 @@
 ﻿using _14_efcore_northwind;
 using _14_efcore_northwind.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 Console.WriteLine("Hello From EFCore!");
 QueryingCategories();
+FilteringQueries();
 
 void QueryingCategories() {
   using(Northwind db = new Northwind()) {
@@ -18,6 +20,32 @@ void QueryingCategories() {
     foreach (Category category in categories)
     {
       Console.WriteLine($"{category.CategoryName} has {category.Products.Count} products.");
+    }
+  }
+}
+
+void FilteringQueries() {
+  using(Northwind db = new ()) {
+    Console.WriteLine("Type the minimum stock number for listing the units in stock.");
+    bool success = Int16.TryParse(Console.ReadLine() ?? "10", out short unitsInStock );
+    if (success) {
+      IIncludableQueryable<Category, IEnumerable<Product>>? categories = db.Categories.Include(c => c.Products.Where(p => p.Stock >= unitsInStock));
+
+      if(categories is null) {
+        Console.WriteLine("No categories found.");
+        return;
+      }
+
+      foreach (Category category in categories) {
+        Console.WriteLine($"{category.CategoryName} has {category.Products.Count} products with a minimum of {unitsInStock} units in stock.");
+
+        foreach (Product product in category.Products) {
+          Console.WriteLine($"    {product.ProductName} has {product.Stock} units in stock");
+        }
+      }
+    } else {
+      Console.WriteLine("Type something more meaningful.");
+      return;
     }
   }
 }
